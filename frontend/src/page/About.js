5df5d2e387f4b49a64af06792fd2846fb38d5edb
Aswathi P J <aswathipjsreenilayam@gmail.com>
2023-06-25 14:36:14 +0530

    more changes to home page

--- frontend/src/page/About.js
+++ frontend/src/page/About.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>LOCALIA - The locality-based delivery system is a website that is intended to provide shopkeepers
+    <div>
+      <p className='mt-5 text-xl'>ABOUT</p>
+      <p className='mt-10'>LOCALIA - The locality-based delivery system is a website that is intended to provide shopkeepers
     with a platform to sell their products online. It also enables customers to search for their needed
     products and purchase them without having to go to the shop physically. Our system also helps
     small-scale entrepreneurs to sell their products through the website. This system also provides
-    employment opportunities for delivery drivers.</div>
+    employment opportunities for delivery drivers.</p>
+      </div>
   )
 }
 
